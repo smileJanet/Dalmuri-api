dfe2fc2cff6c5c27151689308941d237319d48e6
Juyoung <juyeonge6@gmail.com>
2025-07-18 16:16:40 +0900

    settings

--- api/src/main/java/com/dalmuri/config/WebSocketConfig.java
+++ api/src/main/java/com/dalmuri/config/WebSocketConfig.java
@@ -7,9 +7,38 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    /*
+    * 1) @Configuration : 해당 클래스가 스프링의 설정(Configuration)파일이라는 뜻. 스프링은 이 클래스를 읽어서 애플리케이션을 어떻게 구성할지 파악한다.
+    * "이 파일에 중요한 설정 정보가 있으니까 잘 봐둬!"
+    * 2) @EnableWebSockt : 스프링에게 웹소켓 기능을 활성화하라고 지시. 스프링이 웹소켓 관련 빈(Bean)들을 자동으로 등록하고, 웹소켓 통신을 처리할 준비를 할 수 있다.
+    * "이제부터 웹소켓 통신을 사용할 거니까 준비해 둬!"
+    *
+    * */
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+        /*
+        * 1) @Override : 현재 메소드가 부모 인터페이스의 메소드를 "재정의"하고 있다는 뜻.
+        * 2) WebSocketConfigurer : 웹소켓 핸들러(Handler)를 등록하는 방법을 제공
+        * "나는 웹소켓을 설정하는 규칙에 따라 코드를 작성할거야."
+        * + implements"구현" : 구현한다는 것은 WebSocketConfigurer가 정의한 메소드를 "반드시" 구현할 것이라는 의미.
+        * 왜 "반드시" 구현하냐면, 부모에게 해당 클래스가 정의되어있지 않기 때문.
+        *
+        * [갑분 extends, implements]
+        * extends = "기능을 그대로 물려받는다" (부모가 메서드를 이미 가지고 있음)
+        * implements = "기능을 설계도로 받아서 내가 직접 만든다" (부모가 메서드의 이름만 알려줌)
+        *
+        * */
         registry.addHandler(new ChatHandler(), "/chat").setAllowedOrigins("*");
+
+        /*
+        * 1) addHandler : 웹소켓 핸들러 추가
+        * 2) new ChatHandler : ChatHandler(임의로 작성)라는 웹소켓 핸들러의 새로운 인스턴스를 생성. ChatHandler에 실제로 웹소켓 메시지를 주고받는 로직(=채팅 처리 로직)을 담고 있을 것이다.
+        * 3) /chat : 클라이언트 측에서 웹소켓 연결을 요청할 "경로"를 지정. ex : ws://localhost:8080/chat과 같은 주소로 웹소켓 연결을 시도한다면 ChatHandler가 이를 처리할 것이다.
+        * 4) setAllowedOrigins("*") : 교차 출처(Cross Origin)요청을 허용하는 설정. *는 모든 출처(origin)에서의 접근을 허용하겠다는 의미. 단 보안상 권장되지 않으며, 실제 운영에선 특정 도메인만 허용되도록 설정할 것
+        *   ex: "http://localhost:3000", "https://yourdomain.com"
+        *
+        * */
     }
 
 }
--- api/src/main/java/com/dalmuri/socket/ChatHandler.java
+++ api/src/main/java/com/dalmuri/socket/ChatHandler.java
@@ -9,21 +9,40 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ChatHandler extends TextWebSocketHandler {
+    /*
+    * 1) TextWebSocketHandler : 스프링에서 텍스트 기반의 웹소켓 메시지를 처리하는데 필요한 기본적인 기능들을 미리 구현해 놓은 추상 클래스
+    * + extends"확장" : 부모가 추상클래스일 때 붙임. 부모로부터 기능을 "물려받을 때". 부모에게 기능을 상속받음(=재사용)
+    *
+    * [갑분 extends, implements]
+    * extends = "기능을 그대로 물려받는다" (부모가 메서드를 이미 가지고 있음)
+    * implements = "기능을 설계도로 받아서 내가 직접 만든다" (부모가 메서드의 이름만 알려줌)
+    *
+    * */
+
     private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    /*
+    * 1) Set : 웹소켓에 연결된 모든 클라이언트의 세션 정보(WebSocketSession)를 저장하는 공간을 제공.
+    * Set은 순서 유지도 없고(인덱스 안씀), 중복도 안되서 많이 안쓰긴 한다...
+    * 2) private : 이 sessions라는 변수가 ChatHandler클래스 안에서만 접근 가능하다는 의미
+    * 3) final : 한번 초기화되면 다시 할당할 수 없음을 의미
+    *
+    * */
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session);
-        System.out.println("🟢 연결됨: " + session.getUri());
+//        System.out.println("🟢 연결됨: " + session.getUri());
     }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("💬 받은 메시지: " + message.getPayload());
+//        System.out.println("💬 받은 메시지: " + message.getPayload());
 
-        // 연결된 모든 세션에 메시지 전송
+        // 연결된 모든 세션에 메시지 전송 (단, 메시지를 보낸 본인은 제외)
         for(WebSocketSession s : sessions){
-            if(s.isOpen()){
+//            System.out.println("너 누구냐 : " + s.getId() + ", 세션은 누구냐 : " + session.getId());
+            if(s.isOpen() && !s.getId().equals(session.getId())){
                 s.sendMessage(message);
             }
         }
@@ -32,7 +51,7 @@ public class ChatHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception{
         sessions.remove(session);
-        System.out.println("🔴 연결 종료!");
+//        System.out.println("🔴 연결 종료!");
     }
 
 
