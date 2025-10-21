022785f43bda08778f9c3a07f22f6dbffcdbbcc0
Juyoung <juyeonge6@gmail.com>
2025-10-21 10:50:22 +0900

    refactor : refactored controller/service

--- api/src/main/java/com/dalmuri/dmr/web/diary/controller/DiaryController.java
+++ api/src/main/java/com/dalmuri/dmr/web/diary/controller/DiaryController.java
@@ -2,16 +2,15 @@ package com.dalmuri.dmr.web.diary.controller;
 
 import com.dalmuri.dmr.web.diary.model.DiaryRequestDTO;
 import com.dalmuri.dmr.web.diary.model.DiaryResponseDTO;
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.Document.Type;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
+import com.dalmuri.dmr.web.diary.service.DiaryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins="http://localhost:1003")
 @RequestMapping("/diary")
+@RequiredArgsConstructor
 public class DiaryController {
 
     /*
@@ -89,58 +88,39 @@ public class DiaryController {
     *
     * */
 
+    private final DiaryService service;
+
     /*
-    * [try-with-resources 구문]
-    * try-catch 구문에선
-    * try - catch - "finally"를 무조건 명시해야 한다. 메모리 누수를 위해.... (ex. stream.close()와 같이)
-    * 근데 try-with-resources는 finally를 굳이 쓰지 않아도 된다. 자원을 자동으로 관리하고 해제해주기 때문
-    * 내가 여태 쓴게 try-with-resources였다. try-catch가 아니라
+    * [Constructor]
     *
-    * 주로 finally는 예외가 발생하든, 발생하지 않든 무조건 처리해야 하는 로직이 있을 때 사용한다.
-    * ex. 자원 해제 : 파일, 네트워크, DB연결 후 반납해야 할 때
-    *     정리 작업 : 예외가 발생하더라도 반드시 수행해야 하는 로직이 있을 때
-    *     보장된 실행 : try구문에서 예외가 실행되든, 실행되지 않든 반드시 수행해야 하는 로직이 있을 때
+    * (1) RequiredArgsConstructor
+    * 클래스에 선언된 final, @NonNull 필드를 자동으로 초기화해주는 생성자를 의미
+    * ex) public DiaryController (DiaryService service) { this.service = service; }
+    * '필수 필드만' 매개변수 생성자를 대신 만들어줌
+    * final : 객체 생성 시 반드시 주입되어야 함, 이후에 변경될 수 없음 '불변 의존성'
+    *
+    * * 헷갈림 주의 !
+    * 상수(constant) : public static final ~ 이라고 선언됨
+    * 상수는 **프로그램 전체**에서 고정된 값
+    *
+    * (2) NoArgsConstructor
+    * 기본 생성자를 대신 만들어주는 어노테이션
+    * ex) public DiaryController () {}
+    * 아무 인자도 없는 빈 생성자를 만들어줌
+    * JPA Entity 직렬화, 프레임워크에서 객체를 자동 생성할 때 유용함
+    *
+    * (3) AllArgsConstructor
+    * ex) public DiaryController (DiaryService service) { this.service = service; }
+    * 클래스 내에 있는 모든 필드를 매개변수로 받는 전체 매개변수 생성자를 만듦
+    * DTO와 같은 단순 데이터 객체를 만들 때 자주 사용
+    *
     * */
+
 
     @PostMapping("/get-diary-score")
     public ResponseEntity<DiaryResponseDTO> getDiaryScore(@RequestBody DiaryRequestDTO request) {
-        DiaryResponseDTO response = new DiaryResponseDTO();
+        return service.getDiaryScore(request);
+    } // getDiaryScore
 
-        try (LanguageServiceClient language = LanguageServiceClient.create()) {
-
-            String text = request.getText();
-            Document doc = Document.newBuilder()    // Google Cloud Natural Language API에 넘겨줄 Document 객체 생성
-                    .setContent(text)               // 분석할 실제 텍스트 설정
-                    .setType(Type.PLAIN_TEXT)       // 분석 대상이 일반 텍스트임을 지정
-                    .build();                       // 최종 Document 객체 생성
-
-            Sentiment sentiment = language.analyzeSentiment(doc)    // Google Cloud에 감정 분석 요청을 보냄. 텍스트의 감정을 분석해서 점수를 메기고 반환
-                    .getDocumentSentiment();                        // 문서 전체에 대한 평균 감정 점수(Sentiment)를 가져옴
-
-            /*
-            * [구글에서 제공하는 감정 점수(Sentiment)의 범위]
-            * 출처 : https://cloud.google.com/natural-language/docs/basics?hl=ko#interpreting_sentiment_analysis_values
-            * 1) score : -1.0 → 매우 부정적, 0.0 → 중립, +1.0 → 매우 긍정적
-            * 2) magnitude : 감정의 강도 (0.0 이상), 높을수록 감정의 강도가 큼 (기쁨, 분노, 슬픔 등 강렬한 감정 표현)
-            * */
-
-            response.setScore(sentiment.getScore());
-            response.setMagnitude(sentiment.getMagnitude());
-
-            return ResponseEntity.ok(response);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Google Cloud API 호출 중 예외 발생: " + e.getMessage());
-
-            return ResponseEntity.badRequest().build();
-        }
-
-//        [TEST LOGIC]
-//        response.setScore(0.8f);
-//        response.setMagnitude(0.8f);
-//        return ResponseEntity.ok(response);
-
-    }
 
 }
